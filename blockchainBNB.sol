6985e755199fb2c0edf48e8ac07ead6332350b55
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-11-14 09:47:31 -0500

    Create blockchainBNB.sol

---
+++ blockchainBNB.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+/// @title Simple DAO smart contract.
+contract blockchainBNB {
+    
+    struct property {
+        uint256 perNight;
+        bool paid;  // if true, that person already voted
+        bool available;
+
+    }
+
+
+    mapping(address => property) public properties;
+    
+    address[] public owners;
+    
+    uint256 private fee;
+    
+    
+    
+    function listProperty(uint256 perNight) public {
+        
+        // add owner address to list owners
+        owners.push(msg.sender);
+        
+        
+        // adding values to protery struct
+        properties[msg.sender].perNight = perNight;
+        
+        properties[msg.sender].paid = false;
+        
+        properties[msg.sender].available = true;
+
+        
+    }
+    
+    
+    function rentProperty(address payable owner, uint256 nights) public payable returns (uint256) {
+        
+        // calculating fee by calling properties mapping
+        fee = nights * properties[owner].perNight;
+        
+        // require msg.value to be greater than or equal to price per night
+        require (msg.value >= fee);
+        
+        properties[msg.sender].paid = true;
+        
+        properties[msg.sender].available = false;
+        
+        owner.transfer(fee);
+        
+        return fee;
+        
+        
+    }
+    
+}
+
+
+    

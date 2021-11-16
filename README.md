87a9ae6d41143a07676d39227842121c0aa39b98
partylikeits1983 <aleqvids@gmail.com>
2021-11-16 13:31:17 +0300

    updates

---
+++ README.md
@@ -0,0 +1,33 @@
+## This is a smart contract that allows users to list and rent properties on the blockchain. The smart contract is designed to function as the payment backend. 
+
+please feel free to make a pull request!
+
+------------------------
+
+#### future functions:
+
+#### stage 1
+
+function:  update rental price (completed)
+struct mapping: owner can have multiple rental properties (in progress)
+function: pay half to reserve, pay in full on arrival (in progress)
+function: security deposit (in progress)
+
+#### stage 2
+
+list past of renters of owner
+struct: owners can have ratings 
+struct: renters can have ratings
+
+#### stage 3 
+
+add temporality to rentals... without this the contract is useless...
+
+
+#### stage 4
+
+enable payment in ERC20 ()
+
+#### stage 5
+
+enable commission that is split among token holders 
--- contracts/blockchainBNB.sol
+++ contracts/blockchainBNB.sol
@@ -14,32 +14,64 @@ contract blockchainBNB {
 
         // pay in full or 50% now 50% on arrival
         bool payInFull;
+        uint256 securityDeposit;
 
     }
 
+    // still trying to figure out how to have multiple properties per address...
+    struct locations {
+        uint256[] listofproperties;
 
+    }
+
+    struct OWNER_RATINGS {
+        address owner;
+        uint256 rating;
+        address[] renters;
+    }
+
+    struct RENTER_RATINGS {
+        address renter;
+        uint256 rating;
+    }
+
+
+    
     mapping(address => property) public properties;
+
+
+    mapping(address=>uint256) security;
+
     
     // this list of addresses is for future features - mainly if an owner wants to delete all of their listed properties 
     address[] public owners;
     
     uint256 private fee;
-      
+
+    uint256 private payment;
+
+    uint256 private securityDeposit;
+
     address private renter;
 
 
-    // future functions:
+    // need to use events instead...
 
-    // function:  update rental price
-    // struct mapping: owner can have multiple rental properties
-    // function: pay half to reserve, pay in full on arrival 
-    // struct: owners can have ratings
-    // struct: renters can have ratings
-    // function: security deposit
-    // enable payment in ERC20 
+    bool private listed;
+    bool private rented;
 
 
-    function listProperty(uint256 perNight) public {
+    // rating functionality: 
+
+    //mapping(address => OWNER_RATINGS) public ownerRatings;
+
+    //mapping(address => RENTER_RATINGS) public renterRatings;
+
+    //address[] private renters;
+
+
+
+    function listProperty(uint256 perNight, bool payInFull, uint256 SD) public returns (bool) {
         
         // add owner address to list owners
         owners.push(msg.sender);
@@ -56,20 +88,37 @@ contract blockchainBNB {
         
         properties[msg.sender].available = true;
 
+
+        properties[msg.sender].payInFull = payInFull;
+
+        properties[msg.sender].securityDeposit = SD;
+
+        listed = true;
+
+        return listed;
+
     }
     
     
-    function rentProperty(address payable owner, uint256 nights) public payable returns (uint256) {
+    function rentProperty(address payable owner, uint256 nights) public payable returns (uint256, bool) {
         
         renter = msg.sender;
 
         // calculating fee by calling properties mapping
         // @dev eventually needs to be calculated using block timestamp 
-        fee = nights * properties[owner].perNight;
-        
+        fee = (nights * properties[owner].perNight) + properties[owner].securityDeposit;
+
+        payment = nights * properties[owner].perNight;
+
+        securityDeposit = properties[owner].securityDeposit;
+
+
+        // need to add time lock on deposit if there are no disputes 
+        // security deposit can be == 0 
+        security[owner] += securityDeposit;
+
+
         // require msg.value to be greater than or equal to price per night
-
-
         require (msg.value >= fee);
 
         // this is for future features 
@@ -81,14 +130,16 @@ contract blockchainBNB {
         
         properties[owner].available = false;
         
-        owner.transfer(fee);
-        
-        return fee;
-        
+        owner.transfer(payment);
+
+        rented = true;
+
+        return (fee, rented);
         
     }
 
 
+    // update price per night 
     function updateRentalPrice(uint256 newPrice) public returns (uint256) { 
         
         require (msg.sender == properties[msg.sender].owner);
@@ -99,6 +150,4 @@ contract blockchainBNB {
 
     }
 
-
-   
 }
